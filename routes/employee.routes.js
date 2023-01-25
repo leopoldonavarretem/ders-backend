2001800fb117b948aa709c214b67f074147814ec
Leopoldo Navarrete Maravilla <Leopoldonavarretem@outlook.com>
2023-01-25 09:33:14 -0800

    Feature: User can submit tickets.
    Users are able to submit tickets to /employees/tickets. The data is
    validated.

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const logger = require('../config/logger.config');
 const jwtUtil = require('../utility/jwt.util');
 const userDao = require("../dao/user.dao");
+const ticketDao = require('../dao/ticket.dao');
 
 //This route allows users to change their information.
 //TODO: Users can add a profile picture.
@@ -44,9 +45,54 @@ router.get('/tickets', (req, res) => {
 });
 
 //This route allows users to submit a ticket.
-router.post('/tickets', (req, res) => {
+router.post('/tickets', async (req, res) => {
     logger.info(`${req.method} received to ${req.url}.`);
-    res.send({message: "You have accessed the POST /tickets route."});
+    try {
+        const authorizationHeader = req.headers.authorization;
+        const token = authorizationHeader.split(" ")[1];
+        const tokenPayload = await jwtUtil.verifyTokenAndReturnPayload(token);
+
+
+        const {amount, description, title} = req.body;
+        const type = req.body.type.toLowerCase();
+
+        if (!amount || !description || !type || !title) {
+            res.status(400);
+            res.send({
+                "message": "Please input all required parameters."
+            });
+        } else if (amount <= 0) {
+            res.status(400);
+            res.send({
+                "message": "Amount cannot be less than $1"
+            });
+        } else if (type !== "food" && type !== 'lodging' && type !== 'travel' && type !== "other") {
+            res.status(400);
+            res.send({
+                "message": "Type must be either food, travel, lodging or other."
+            });
+        } else {
+            await ticketDao.submitTicket(amount, description, type, title, tokenPayload.username);
+
+            res.send({"message": "Ticket successfully created."});
+        }
+
+
+    } catch (err) {
+        if (err.name === 'JsonWebTokenError') {
+            res.status(400);
+            res.send({
+                "message": "Invalid JWT"
+            });
+        } else if (err.name === 'TypeError') {
+            res.status(400);
+            res.send({
+                "message": "No authorization header provided"
+            });
+        } else {
+            res.status(500);
+        }
+    }
 });
 
 router.get('/tickets/:ticketId', (req, res) => {
